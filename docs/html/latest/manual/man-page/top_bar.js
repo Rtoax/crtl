fc72153ade94027e13818933e24d92f428fe1031
root <root@localhost.localdomain>
2020-04-14 08:41:13 +0800

    add docs -> crtl_application_xxxx

---
+++ docs/html/latest/manual/man-page/top_bar.js
@@ -0,0 +1,19 @@
+//<!-- 这是crtl_XXX.html footer -->
+
+
+var include_test = function(name) {
+	var xhr = new XMLHttpRequest(),
+		okStatus = document.location.protocol === "file"?0:200;
+	xhr.open('GET', name, false);
+	xhr.overrideMimeType("text/html;charset=utf-8");
+	xhr.send(null);
+	return xhr.status === okStatus?xhr.responseText:null;
+};
+
+document.write(include_test('./top_bar.html'));
+
+
+
+
+
+
