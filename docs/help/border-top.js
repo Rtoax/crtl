a8af02e887c81df91868a99f371ab9a907374b3d
root <root@localhost.localdomain>
2020-04-18 15:26:14 +0800

    crtl help page

---
+++ docs/help/border-top.js
@@ -0,0 +1,13 @@
+var include_test = function(name) {
+	var xhr = new XMLHttpRequest(),
+		okStatus = document.location.protocol === "file"?0:200;
+	xhr.open('GET', name, false);
+	xhr.overrideMimeType("text/html;charset=utf-8");
+	xhr.send(null);
+	return xhr.status === okStatus?xhr.responseText:null;
+};
+
+document.write(include_test('./border-top.html'));
+
+
+
